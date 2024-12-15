0ad610450a5e05b7815a1e419c66fa508cb8db4a
AngusB00 <blackclover2221@gmail.com>
2024-12-15 19:06:29 +0700

    ex step 2

---
+++ ex9.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Itim&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+<style>
+    <body> {
+  font-family: "Noto Sans Thai", sans-serif;
+  font-style: normal;
+
+}
+</style>
+    </body>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ตัวอย่าง 2 </title>
+</head>
+
+<body>
+    <h1> โปรแกรมคำนวณพื้นที่สี่เหลี่ยม </h1>
+    <?php
+        $w=$_POST["width"];
+        $h=$_POST["height"];
+        $t=$_POST["thick"];
+        $sum=$w*$h*$t*0.01;
+        echo " ความกว้าง ".$w." ความยาว ".$h." ความหนา ".$t." พื้นที่เท่ากับ ".$sum;
+
+    ?>
+</body>
+</html>
